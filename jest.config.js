0191dd2152dd077bde4dffdc06589ed92a594cd3
Rob van der Leek <robvanderleek@gmail.com>
2023-07-04 19:19:59 +0200

    fix: Fix unit-tests

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 export default {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    moduleNameMapper: {
+        "^(\\.\\.?\\/.+)\\.jsx?$": "$1"
+    },
 };
--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,4 +1,4 @@
-import {printSuccess} from "./utils";
+import {printSuccess} from "../src/utils.js";
 
 test('print success text', async () => {
     printSuccess('Hello world');
